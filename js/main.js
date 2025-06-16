85c650fbae837dd40600be45a2362f4befc41fcd
Aman Nurgozhiyev <anurgozhiyev@gmail.com>
2025-06-16 11:37:34 +0500

    feat: added burger menu functionality with storing its state in localstorage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 import { ThemeToggle } from "./utils/utils.js";
+import { ViewToggle } from "./utils/utils.js";
+import { BurgerMenu } from "./utils/utils.js";
 
 document.addEventListener("DOMContentLoaded", ({
 
 
 }));
 
+//Theme selection functionality 
 ThemeToggle();
+
+//View selection functionality 
+ViewToggle();
+
+//Burger menu functionality for large window sizes
+BurgerMenu();
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,20 +1,63 @@
-function ThemeToggle(){
 
-const selectedTheme = document.getElementById("theme-toggle");
+//Theme selection functionality 
+function ThemeToggle() {
 
-const savedTheme = localStorage.getItem("theme-preference");
+    const themeToggleChkBox = document.getElementById("theme-toggle");
 
-if(savedTheme === "false"){
-     selectedTheme.checked = false;
-} else {
-    selectedTheme.checked = true;
+    //get 
+    const prefTheme = localStorage.getItem("theme-preference");
+
+    //check 
+    if (prefTheme === "true") {
+        themeToggleChkBox.checked = true;
+    } else {
+        themeToggleChkBox.checked = false;
+    }
+
+    themeToggleChkBox.addEventListener("change", function () {
+        localStorage.setItem("theme-preference", themeToggleChkBox.checked)
+    })
+
 }
 
-selectedTheme.addEventListener("change", function(){
-    localStorage.setItem("theme-preference", selectedTheme.checked)
-})
-  
+//Task view functionality
+function ViewToggle() {
+
+}
+
+function BurgerMenu() {
+
+    const burgerMenuChkBox = document.getElementById("burger-menu-toggle");
+    const navbar = document.getElementsByTagName("nav")
+    const prefState = localStorage.getItem("burger-menu-preference");
+
+    if (prefState === "true") {
+        burgerMenuChkBox.checked = true;
+        navbar[0].classList.add("burger-menu-active")
+        navbar[0].classList.remove("burger-menu-inactive")
+    } else {
+        burgerMenuChkBox.checked = false;
+        navbar[0].classList.remove("burger-menu-active")
+        navbar[0].classList.add("burger-menu-inactive")
+
+    }
+
+    burgerMenuChkBox.addEventListener("change", function () {
+
+        if (burgerMenuChkBox.checked) {
+            navbar[0].classList.add("burger-menu-active")
+            navbar[0].classList.remove("burger-menu-inactive")
+        } else {
+            navbar[0].classList.remove("burger-menu-active")
+            navbar[0].classList.add("burger-menu-inactive")
+        }
+
+        //stores new value of burger menu checkbox when checkbox is triggered
+        localStorage.setItem("burger-menu-preference", burgerMenuChkBox.checked);
+
+    })
+
 }
 
 
-export {ThemeToggle};
+export { ThemeToggle, ViewToggle, BurgerMenu };
