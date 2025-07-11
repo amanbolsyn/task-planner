8889ceab2e77bd06cc6bd87a4a13e431ae6256cd
Aman Nurgozhiyev <anurgozhiyev@gmail.com>
2025-07-11 20:28:12 +0500

    feat: adjustable commits(not ready yet)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import { ThemeToggle } from "./utils/utils.js";
 import { ViewToggle } from "./utils/utils.js";
 import { BurgerMenu } from "./utils/utils.js";
 
+import { CreateTaskForm } from "./utils/utils.js";
+
+
 document.addEventListener("DOMContentLoaded", ({
 
 
@@ -10,8 +13,10 @@ document.addEventListener("DOMContentLoaded", ({
 //Theme selection functionality 
 ThemeToggle();
 
+//Burger menu functionality for large window sizes
+BurgerMenu();
+
 //View selection functionality 
 ViewToggle();
 
-//Burger menu functionality for large window sizes
-BurgerMenu();
+CreateTaskForm();
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -20,44 +20,106 @@ function ThemeToggle() {
 
 }
 
-//Task view functionality
+//Task view(column or list) functionality
 function ViewToggle() {
+
+    const viewToggleChkBx = document.getElementById("view-toggle");
+    const viewPref = localStorage.getItem("view-preference");
+ 
+    const taskWindow = document.querySelector(".main-task-cards");
+
+    if (viewPref === "false") {
+        viewToggleChkBx.checked = false;
+        taskWindow.classList.add("column-view")
+        DisplayView();
+     } else {
+        viewToggleChkBx.checked = true;
+         taskWindow.classList.add("list-view")
+     }
+
+    viewToggleChkBx.addEventListener("change", function () {
+
+        if(viewToggleChkBx.checked){
+           taskWindow.classList.add("column-view")
+           taskWindow.classList.remove("list-view")
+           DisplayView(); 
+        } else {
+            taskWindow.classList.add("list-view")
+            taskWindow.classList.remove("column-view")
+        }
+        localStorage.setItem("view-preference", viewToggleChkBx.checked);
+    });
+}
+
+function DisplayView() {
+
+   const taskWindow = document.querySelector(".main-task-cards");
+
+   const windowWidth = taskWindow.offsetWidth;
+   let numOfColumnns = Math.floor(windowWidth/300);
+
+   taskWindow.style.setProperty('--columns', numOfColumnns)
+
+   taskWindow.innerHTML = "";
+
+   for(let i = 0; i<numOfColumnns; i++ ){
+    const div = document.createElement("div");
+    div.className = "column";
+    div.innerText = `column ${i+1}`;
+    taskWindow.appendChild(div)
+   }
 
 }
 
 function BurgerMenu() {
 
     const burgerMenuChkBox = document.getElementById("burger-menu-toggle");
-    const navbar = document.getElementsByTagName("nav")
     const prefState = localStorage.getItem("burger-menu-preference");
+
+    const aside = document.querySelector(".aside")
 
     if (prefState === "true") {
         burgerMenuChkBox.checked = true;
-        navbar[0].classList.add("burger-menu-active")
-        navbar[0].classList.remove("burger-menu-inactive")
+        aside.classList.add("burger-menu-active");
+        aside.classList.remove("burger-menu-inactive");
     } else {
         burgerMenuChkBox.checked = false;
-        navbar[0].classList.remove("burger-menu-active")
-        navbar[0].classList.add("burger-menu-inactive")
+        aside.classList.remove("burger-menu-active");
+        aside.classList.add("burger-menu-inactive");
 
     }
 
     burgerMenuChkBox.addEventListener("change", function () {
 
         if (burgerMenuChkBox.checked) {
-            navbar[0].classList.add("burger-menu-active")
-            navbar[0].classList.remove("burger-menu-inactive")
+            aside.classList.add("burger-menu-active");
+            aside.classList.remove("burger-menu-inactive");
         } else {
-            navbar[0].classList.remove("burger-menu-active")
-            navbar[0].classList.add("burger-menu-inactive")
+            aside.classList.remove("burger-menu-active");
+            aside.classList.add("burger-menu-inactive");
         }
 
         //stores new value of burger menu checkbox when checkbox is triggered
         localStorage.setItem("burger-menu-preference", burgerMenuChkBox.checked);
-
     })
-
 }
 
 
-export { ThemeToggle, ViewToggle, BurgerMenu };
+function CreateTaskForm() {
+
+    const taskForm = document.getElementById("new-task-form")
+    const taskDescription = document.getElementById("task-description");
+    const taskTitle = document.getElementById("task-tittle")
+
+    taskDescription.addEventListener("focus", function () {
+        taskForm.classList.add("task-form-active");
+        taskForm.classList.remove("task-form-inactive");
+
+    })
+
+     taskTitle.addEventListener("focus", function () {
+
+    })
+}
+
+export { ThemeToggle, ViewToggle, BurgerMenu, CreateTaskForm};
